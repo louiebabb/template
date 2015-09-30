c05501a4432335799c887f339630a7ebf34e7ca7
Louie Babb <louie.babb@pulselive.com>
2015-09-30 12:58:05 +0100

    Grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    htmlPath: "src/html/",
     //JS Uglify
     uglify: {
       options: {
@@ -22,13 +21,6 @@ module.exports = function(grunt) {
         dest: '<%= pkg.output %>/js/custom.min.js'
       }
     },
-    //CONCAT 
-    concat: {
-      dist: {
-        src: ['<%= htmlPath %>partials/header.html', 'src/html/pages/*.html', 'src/html/partials/footer.html'],
-        dest: '<%= pkg.output %>/index.html',
-      },
-    },
     //COMPASS
     compass: {
       dev: {
@@ -46,66 +38,84 @@ module.exports = function(grunt) {
     },
     // HTML build
     htmlbuild: {
-        dist: {
-            src: '<%= htmlPath %>/pages/*.html',
-            dest: 'target/',
-            options: {
-                beautify: true,
-                relative: true,
-                sections: {
-                    views: '<%= htmlPath %>/pages/**/*.html',
-                    templates: '<%= htmlPath %>/pages/**/*.html',
-                    layout: {
-                        header: '<%= htmlPath %>/partials/header.html',
-                        footer: '<%= htmlPath %>/partials/footer.html'
-                    }
-                },
+      dist: {
+        src: '<%= pkg.htmlPath %>/pages/*.html',
+        dest: '<%= pkg.output %>',
+        options: {
+          beautify: true,
+          relative: true,
+          sections: {
+            layout: {
+                header: '<%= pkg.htmlPath %>/partials/header.html',
+                footer: '<%= pkg.htmlPath %>/partials/footer.html'
             }
+          },
         }
+      }
     },
-
     //IMAGES
     imagemin: {
-        dynamic: {
+      dynamic: {
         files: [{
           expand: true,                  // Enable dynamic expansion 
           cwd: 'src/i',                   // Src matches are relative to this path 
           src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match 
-          dest: 'target/i'                  // Destination path prefix 
+          dest: '<%= pkg.output %>i'                  // Destination path prefix 
         }]
-        }
+      }
+    },
+    //COPY
+    copy: {
+      images: {
+        files: [
+          { expand: true, src: ['src/i/*.{png,jpg,gif}','src/i/**/*.{png,jpg,gif}'], dest: 'target/i/', filter: 'isFile'},
+        ],
+      },
+      fonts:{
+        fontsCopy: [
+          { expand: true, src: ['src/fonts/*'], dest: 'target/fonts/', filter: 'isFile'},
+        ],
+      },
     },
     // WATCH
     watch: {
-        js: {
-          files:['src/js/*.js'],
-          tasks:['uglify:build']
-        },
-        css: {
-          files: ['src/css/*.scss','src/css/sass/*.scss'],
-          tasks: ['compass:dev'],
-        },
-        html: {
-          files: ['src/html/partials/*.html','src/html/pages/*.html'],
-          tasks: ['htmlbuild'],
-        },
-        images: {
-          files: ['src/i/*.{png,jpg,gif}'],
-          tasks: ['imagemin']
-        }
-    }
+      js: {
+        files:['src/js/*.js'],
+        tasks:['uglify:build']
+      },
+      css: {
+        files: ['src/css/*.scss','src/css/sass/*.scss'],
+        tasks: ['compass:dev'],
+      },
+      html: {
+        files: ['src/html/partials/*.html','src/html/pages/*.html'],
+        tasks: ['htmlbuild'],
+      },
+      images: {
+        files: ['src/i/*.{png,jpg,gif}'],
+        tasks: ['copy:images']
+      },
+      fonts: {
+        files: ['src/fonts/**/*'],
+        tasks: ['copy:fonts']
+      }
+    },
+    //CLEAN
+    clean: ["target/**/*"]
   });
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-html-build');
   grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
     
   //default Grunt task
   grunt.registerTask('default', ['watch']);
-  grunt.registerTask('build', ['uglify:build', 'htmlbuild', 'compass:prod', 'imagemin']);
+  grunt.registerTask('dev', ['uglify:dev', 'htmlbuild', 'compass:dev', 'copy']);
+  grunt.registerTask('prod', ['uglify:build', 'htmlbuild', 'compass:prod', 'imagemin', 'copy:fonts']);
 
 };
 
